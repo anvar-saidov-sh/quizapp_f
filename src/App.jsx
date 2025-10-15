7c1341a2ba903b32af32c1c93297038577a422a8
anvarbeck <anvarsaidov708@gmail.com>
2025-10-15 11:20:44 +0500

    Routing...

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 import './App.css'
+  import { BrowserRouter } from "react-router";
 function App() {
+
+  const root = document.getElementById('root');
+  ReactDOM.createRoot(root).render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+  );
 
   return (
     <>
