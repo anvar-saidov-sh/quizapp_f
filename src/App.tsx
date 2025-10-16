bcb4193287dc68c3c324bac0ad65f11709160306
anvarbeck <anvarsaidov708@gmail.com>
2025-10-16 16:38:35 +0500

    issues with routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-
+import {
+  BrowserRouter,
+  Route,
+  Routes,   
+} from "react-router";
 import './App.css'
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
 
   return (
     <>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes> 
+      </BrowserRouter>
     </>
   )
 }
