a0d4a5698f3c8d56944af7971d96d2b8b133f4f9
anvarbeck <anvarsaidov708@gmail.com>
2025-10-18 14:23:34 +0500

    Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,9 +2,36 @@
 function Footer() {
   return (
     <>
-      <footer>
-        <div className="bg-quizzy-surface dark:bg-quizzy-surface p-4 mt-8">
-          <p className="text-center text-quizzy-text dark:text-quizzy-text">
+      <footer className="flex w-full flex-col px-5 py-4 bg-[#0F172A] text-[#CBD5E1]">
+        <section className="flex  justify-between items-center w-full">
+          <div className="flex flex-col items-start justify-center w-[25%]">
+            <h2 className="text-[25px] bg-clip-text bg-gradient-to-r from-[#fff] to-[#2563EB] font-extrabold">Quizzy</h2>
+            <p>
+              The ultimate quiz platform for students and teachers. Learn, compete, and earn rewards
+            </p>
+          </div>
+          <div className="flex flex-col items-start justify-center">
+            <h2 className="text-[25px] bg-clip-text bg-gradient-to-r from-[#fff] to-[#2563EB] font-extrabold">Quizzy</h2>
+            <p>
+              The ultimate quiz platform for students and teachers. Learn, compete, and earn rewards
+            </p>
+          </div>
+          <div className="flex flex-col items-start justify-center">
+            <h2 className="text-[25px] bg-clip-text bg-gradient-to-r from-[#fff] to-[#2563EB] font-extrabold">Quizzy</h2>
+            <p>
+              The ultimate quiz platform for students and teachers. Learn, compete, and earn rewards
+            </p>
+          </div>
+          <div className="flex flex-col items-start justify-center">
+            <h2 className="text-[25px] bg-clip-text bg-gradient-to-r from-[#fff] to-[#2563EB] font-extrabold">Quizzy</h2>
+            <p>
+              The ultimate quiz platform for students and teachers. Learn, compete, and earn rewards
+            </p>
+          </div>
+        </section>
+
+        <div className=" p-4 mt-8">
+          <p className="text-center">
             &copy; 2025 Quizzy. All rights reserved. | Terms and Conditions | Privacy Policy
           </p>
         </div>
