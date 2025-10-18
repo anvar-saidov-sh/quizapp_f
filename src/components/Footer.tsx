771ce8c52b88640b4a17c879c98cfb0d6b89bf45
anvarbeck <anvarsaidov708@gmail.com>
2025-10-18 14:13:24 +0500

    footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,16 @@
+
+function Footer() {
+  return (
+    <>
+      <footer>
+        <div className="bg-quizzy-surface dark:bg-quizzy-surface p-4 mt-8">
+          <p className="text-center text-quizzy-text dark:text-quizzy-text">
+            &copy; 2025 Quizzy. All rights reserved. | Terms and Conditions | Privacy Policy
+          </p>
+        </div>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
