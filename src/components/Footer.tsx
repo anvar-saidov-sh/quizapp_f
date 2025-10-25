7eafbcddfb3b9ed3feed79c9b416a447d471aa38
anvarbeck <anvarsaidov708@gmail.com>
2025-10-25 18:27:03 +0500

    nothing

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 
-function Footer() {
+export default function Footer() {
     return (
         <>
             <footer className="bg-gray-200 text-center text-gray-700 py-3 dark:bg-gray-800 dark:text-gray-300 mt-4">
@@ -9,4 +9,3 @@ function Footer() {
     )
 }
 
-export default Footer
